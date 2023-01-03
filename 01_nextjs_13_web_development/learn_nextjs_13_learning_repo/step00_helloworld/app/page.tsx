eecdf811c32611c8a7c68ada626dbae8729d8c1e
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-01-03 12:44:46 +0500

    step00_helloworld nextjs

---
+++ 01_nextjs_13_web_development/learn_nextjs_13_learning_repo/step00_helloworld/app/page.tsx
@@ -0,0 +1,14 @@
+// import Image from 'next/image';
+import { Inter } from '@next/font/google';
+// import styles from './page.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <div>
+      <h1>NextJS 13</h1>
+      <p>Hello World from Hassan Ali Khan</p>
+    </div>
+  );
+}
