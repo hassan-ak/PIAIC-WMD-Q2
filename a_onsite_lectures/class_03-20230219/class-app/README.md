7dff7c8b010a756e47117f504608d15329ca8da7
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-02-19 13:17:54 +0500

    UMT class 03 App-Code

---
+++ a_onsite_lectures/class_03-20230219/class-app/README.md
@@ -0,0 +1,3 @@
+# Web3.0 and Metaverse Development
+
+## Class 03 - 2023/03/19
---
+++ a_onsite_lectures/class_03-20230219/class-app/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import panaversePic from '../../public/appLogo.png';
+
+export default function Page() {
+  return (
+    <div className='flex'>
+      <div>
+        <Image src={panaversePic} width={80} height={80} alt='music' />
+      </div>
+      <div>
+        <h1>Welcome to Panaverse</h1>
+        <h2>A Community of Web 3 and Metaverse Developers</h2>
+        <button>Learn More</button>
+      </div>
+    </div>
+  );
+}
