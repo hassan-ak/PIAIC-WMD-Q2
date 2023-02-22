468ad58439791a2313f49c08c0a25c9fbc224ca7
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-02-22 05:03:06 +0500

    Modern CSS with Tailwind Chapter 03 P1

---
+++ 06_nextjs_13_using_tailwindCSS/modern_css_with_tailwind/src/app/chapter03/page.tsx
@@ -0,0 +1,79 @@
+import pLogo from '@/../public/pLogo.png';
+import Image from 'next/image';
+import React from 'react';
+
+export default function Chapter03() {
+  return (
+    <div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <h1 className='my-5 text-center text-5xl font-extrabold'>Chapter 03</h1>
+      <div className='h-2 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <p className='text-xs uppercase italic'>text-xs italic uppercase</p>
+      <p className='text-xm lowercase not-italic'>
+        text-xm not-italic lowercase
+      </p>
+      <p className='text-base capitalize underline'>
+        text-base underline capitalize
+      </p>
+      <p className='text-lg normal-case no-underline'>
+        text-lg no-underline normal-case
+      </p>
+      <p className='text-xl font-black overline'>text-xl overline font-black</p>
+      <p className='text-2xl font-extrabold line-through'>
+        text-2xl line-through font-extrabold
+      </p>
+      <p className='text-3xl font-bold underline decoration-solid'>
+        text-3xl underline decoration-solid font-bold
+      </p>
+      <p className='text-4xl font-semibold underline decoration-double'>
+        text-4xl underline decoration-double font-semibold
+      </p>
+      <p className='text-5xl font-medium underline decoration-dotted'>
+        text-5xl underline decoration-dotted font-medium
+      </p>
+      <p className='text-6xl font-normal underline decoration-dashed'>
+        text-6xl underline decoration-dashed font-normal
+      </p>
+      <p className='text-7xl font-light underline decoration-wavy'>
+        text-7xl underline decoration-wavy font-light
+      </p>
+      <p className='text-8xl font-thin'>text-8xl font-thin</p>
+      <p className='font-hairline text-9xl'>text-9xl font-hairline</p>
+      <div className='h-1 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <h1 className='text-3xl font-semibold'>Title</h1>
+      <h2 className='text-xl'>Subtitle</h2>
+      <div className='h-1 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <h1 className='text-4xl font-bold'>Title</h1>
+      <h2 className='text-2xl font-semibold'>Subtitle</h2>
+      <h3 className='text-lg font-medium italic'>Header</h3>
+
+      <div className='h-1 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div className='h-1 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div className='h-1 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div className='h-1 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </div>
+  );
+}
