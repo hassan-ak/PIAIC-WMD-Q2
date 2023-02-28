b66fdc48a612f5f23fce0247cc932ccf25a8e8f6
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-02-28 23:10:31 +0500

    Modern CSS with Tailwind Chapter 04 P2

--- 06_nextjs_13_using_tailwindCSS/modern_css_with_tailwind/src/app/chapter04/page.tsx
+++ 06_nextjs_13_using_tailwindCSS/modern_css_with_tailwind/src/app/chapter04/page.tsx
@@ -46,6 +46,18 @@ export default function Chapter03() {
       {/*  */}
       {/*  */}
       {/*  */}
+      <div className='bg-gray-50 p-10'>
+        <div className='mb-10'>
+          <button className='mx-10 bg-white p-10 shadow-sm'>One</button>
+          <button className='mx-10 bg-white p-10 shadow-sm'>Two</button>
+          <button className='mx-10 bg-white p-10 shadow-lg'>Three</button>
+        </div>
+        <div>
+          <button className='mx-10 bg-white p-10 shadow-xl'>Four</button>
+          <button className='mx-10 bg-white p-10 shadow-2xl'>Five</button>
+          <button className='mx-10 bg-white p-10 shadow-inner'>Six</button>
+        </div>
+      </div>
       <div className='mb-5 h-2 bg-black'></div>
       {/*  */}
       {/*  */}
