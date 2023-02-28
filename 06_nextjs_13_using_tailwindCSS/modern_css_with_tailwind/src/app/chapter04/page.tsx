d8aec70cbe5900e548551746bb07301063ca2833
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-02-28 23:06:20 +0500

    Modern CSS with Tailwind Chapter 04 P1

---
+++ 06_nextjs_13_using_tailwindCSS/modern_css_with_tailwind/src/app/chapter04/page.tsx
@@ -0,0 +1,79 @@
+import pLogo from '@/../public/pLogo.png';
+import Image from 'next/image';
+import React from 'react';
+
+export default function Chapter03() {
+  return (
+    <div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <h1 className='my-5 text-center text-5xl font-extrabold'>Chapter 04</h1>
+      <div className='mb-5 h-2 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div>
+        <div className='mb-10'>
+          <button className='border border-black p-10'>One</button>
+          <button className='m-10 border border-black'>Two</button>
+          <button className='m-2 border-4 border-black p-2'>Three</button>
+        </div>
+        <div>
+          <button className='m-4 rounded-md border-2 border-black p-4'>
+            Four
+          </button>
+          <button className='m-4 rounded-2xl border-2 border-black p-4'>
+            Five
+          </button>
+          <button className='m-4 rounded-full border-2 border-black p-4'>
+            Six
+          </button>
+        </div>
+      </div>
+      <div className='mb-5 h-2 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div>
+        <button className='m-4 rounded-md p-4 ring'>Four</button>
+        <button className='m-4 rounded-2xl p-4 ring-2'>Five</button>
+        <button className='m-4 rounded-full p-4 ring-4 ring-offset-4 ring-offset-black'>
+          Six
+        </button>
+      </div>
+      <div className='mb-5 h-2 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div className='mb-5 h-2 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div className='mb-5 h-2 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div className='mb-5 h-2 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div className='mb-5 h-2 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div className='mb-5 h-2 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div className='mb-5 h-2 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <div className='mb-5 h-2 bg-black'></div>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </div>
+  );
+}
