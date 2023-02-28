b8ecd4c86e3aa9b46b3c78849626582eea5f1948
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-02-28 23:32:12 +0500

    modern_css_with_tailwind

--- 06_nextjs_13_using_tailwindCSS/modern_css_with_tailwind/src/app/page.tsx
+++ 06_nextjs_13_using_tailwindCSS/modern_css_with_tailwind/src/app/page.tsx
@@ -1,7 +1,26 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div>
-      <h1>Modern CSS with Tailwind</h1>
+      <h1 className='my-5 text-center text-5xl font-extrabold'>
+        Modern CSS with Tailwind
+      </h1>
+      <div className='mb-5 h-2 bg-black'></div>
+      <ul className='list-decimal px-10'>
+        <li>
+          <Link href='chapter01'>Chapter 01</Link>
+        </li>
+        <li>
+          <Link href='chapter02'>Chapter 02</Link>
+        </li>
+        <li>
+          <Link href='chapter03'>Chapter 03</Link>
+        </li>
+        <li>
+          <Link href='chapter04'>Chapter 04</Link>
+        </li>
+      </ul>
     </div>
   );
 }
