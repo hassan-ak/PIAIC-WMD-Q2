6078bf06ff49a254396f859d33c7cb80855a9d13
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-03-01 23:17:56 +0500

    css_flex_grid_tailwind_css-p2-2

--- 06_nextjs_13_using_tailwindCSS/css_flex_grid_tailwind_css/src/app/chapter02/page.tsx
+++ 06_nextjs_13_using_tailwindCSS/css_flex_grid_tailwind_css/src/app/chapter02/page.tsx
@@ -40,6 +40,180 @@ export default function Chapter01() {
       {/*  */}
       {/*  */}
       {/*  */}
+      <h1 className='text-2xl font-bold'>Justify Start</h1>
+      <div className='mb-2 bg-purple-200 p-12 text-center font-sans'>
+        <div className='flex justify-start rounded-full bg-white p-2'>
+          <a
+            href='#'
+            className='rounded-full bg-blue-500 py-2  px-6 text-white '
+          >
+            Profile
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Notifications
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Payments
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Settings
+          </a>
+        </div>
+      </div>
+      <h1 className='text-2xl font-bold'>Justify End</h1>
+      <div className='mb-2 bg-purple-200 p-12 text-center font-sans'>
+        <div className='flex justify-end rounded-full bg-white p-2'>
+          <a
+            href='#'
+            className='rounded-full bg-blue-500 py-2  px-6 text-white '
+          >
+            Profile
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Notifications
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Payments
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Settings
+          </a>
+        </div>
+      </div>
+      <h1 className='text-2xl font-bold'>Justify Center</h1>
+      <div className='mb-2 bg-purple-200 p-12 text-center font-sans'>
+        <div className='flex justify-center rounded-full bg-white p-2'>
+          <a
+            href='#'
+            className='rounded-full bg-blue-500 py-2  px-6 text-white '
+          >
+            Profile
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Notifications
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Payments
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Settings
+          </a>
+        </div>
+      </div>
+      <h1 className='text-2xl font-bold'>Justify Between</h1>
+      <div className='mb-2 bg-purple-200 p-12 text-center font-sans'>
+        <div className='flex justify-between rounded-full bg-white p-2'>
+          <a
+            href='#'
+            className='rounded-full bg-blue-500 py-2  px-6 text-white '
+          >
+            Profile
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Notifications
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Payments
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Settings
+          </a>
+        </div>
+      </div>
+      <h1 className='text-2xl font-bold'>Justify Around</h1>
+      <div className='mb-2 bg-purple-200 p-12 text-center font-sans'>
+        <div className='flex justify-around rounded-full bg-white p-2'>
+          <a
+            href='#'
+            className='rounded-full bg-blue-500 py-2  px-6 text-white '
+          >
+            Profile
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Notifications
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Payments
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Settings
+          </a>
+        </div>
+      </div>
+      <h1 className='text-2xl font-bold'>Justify Evenly</h1>
+      <div className='bg-purple-200 p-12 text-center font-sans'>
+        <div className='flex justify-evenly rounded-full bg-white p-2'>
+          <a
+            href='#'
+            className='rounded-full bg-blue-500 py-2  px-6 text-white '
+          >
+            Profile
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Notifications
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Payments
+          </a>
+          <a
+            href='#'
+            className='rounded-full bg-blue-50 py-2 px-6 text-gray-800'
+          >
+            Settings
+          </a>
+        </div>
+      </div>
       <div className='my-5 h-1 bg-black'></div>
       {/*  */}
       {/*  */}
