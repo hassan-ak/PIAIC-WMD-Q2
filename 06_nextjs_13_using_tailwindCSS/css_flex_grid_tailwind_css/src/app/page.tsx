d7b6bd1f43a13e0590b0ac76f668f9d68a4c5d28
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-03-01 18:22:27 +0500

    css_flex_grid_tailwind_css

---
+++ 06_nextjs_13_using_tailwindCSS/css_flex_grid_tailwind_css/src/app/page.tsx
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className='my-5 text-center text-5xl font-extrabold'>
+        CSS Flex & Grid
+      </h1>
+      <div className='mb-5 h-2 bg-black'></div>
+      {/* <ul className='list-decimal px-10'>
+        <li>
+          <Link href='chapter01'>Chapter 01</Link>
+        </li>
+        <li>
+          <Link href='chapter02'>Chapter 02</Link>
+        </li>
+        <li>
+          <Link href='chapter03'>Chapter 03</Link>
+        </li>
+        <li>
+          <Link href='chapter04'>Chapter 04</Link>
+        </li>
+      </ul> */}
+    </div>
+  );
+}
