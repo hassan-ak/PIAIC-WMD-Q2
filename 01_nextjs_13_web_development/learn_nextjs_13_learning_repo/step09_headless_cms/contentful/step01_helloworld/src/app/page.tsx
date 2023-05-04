e73da1f0c80e8322edfb3060eb9e9fe04b537bf4
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-05-04 18:15:48 +0500

    step01_helloworld

---
+++ 01_nextjs_13_web_development/learn_nextjs_13_learning_repo/step09_headless_cms/contentful/step01_helloworld/src/app/page.tsx
@@ -0,0 +1,24 @@
+async function getBlogs() {
+  const res = await fetch(
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/entries?access_token=${process.env.CONTENTFUL_ACCESS_KEY}&content_type=blog`
+  );
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const blogs = await getBlogs();
+  return (
+    <ul>
+      {blogs.items.map((item: any, index: any) => (
+        <li key={index}>{item.fields.title}</li>
+      ))}
+    </ul>
+  );
+}
